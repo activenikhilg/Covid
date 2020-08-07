a57e300cab74f318476e1eb0fd5be15938ff48c3
Nikhil Kumar Gupta <activenikhilg@gmail.com>
2020-08-07 14:07:14 +0530

    added data saving method

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
 import src.visualize as viz
 
 server = viz.app.server
+import src.getData as gd
+
+if (os.path.isdir("data")):
+    _,_ = gd.updateGlobalData()
+else:
+    os.mkdir("data")
+    _,_ = gd.updateGlobalData()
 
 if __name__ == '__main__':
     viz.app.run_server(debug=True)
--- src/getData.py
+++ src/getData.py
@@ -2,14 +2,24 @@ import requests
 import pandas as pd
 import json
 import iso3166
+import os
 
 iso2_iso3 = iso3166.countries_by_alpha2
 
-def getGlobalData():
+def updateGlobalData():
     url = "https://api.covid19api.com/summary"
     data = requests.get(url)
     data = json.loads(data.text)
     df_countries = pd.DataFrame(data["Countries"])
     df_countries["CountryCode"] = df_countries["CountryCode"].apply(lambda x:iso2_iso3[x].alpha3)
     global_data = data["Global"]
+    df_countries.to_csv("data/countriesdata.csv")
+    with open("data/globaldata.json", "w") as f:
+        f.write(json.dumps(global_data))
+    return (df_countries,global_data)
+
+def getGlobalData():
+    df_countries = pd.read_csv("data/countriesdata.csv")
+    with open("data/globaldata.json", "r") as f:
+        global_data = json.loads(f.read())
     return (df_countries,global_data)
